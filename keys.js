f5d6bf2966e41d71d20c051786a12e0396a55a95
James Davis <james.jpdavis@gmail.com>
2016-08-11 22:52:26 -0400

    removed a few things

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: '24Ha1v3nNXfbBg5d8RMWjv5ui',
-  consumer_secret: 'OXqteUvdW2nHEYpmjGPJNUBIVTxvWYqQ1SBhA865Z1Xc9nlMQ5',
-  access_token_key: '4858020490-a0gugJCpYJQMQJXQA2Yy0UhCrGGb1YM2yt89G3A',
-  access_token_secret: 'kVJlCONHWwnPJhBFL2J7NkTACMs0adRG2odsLRZDkWPOK',
+  consumer_key: 'your key',
+  consumer_secret: 'your key',
+  access_token_key: 'your key',
+  access_token_secret: 'your key',
 };
