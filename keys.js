b7388bcfd0cef171e889e18236a4e5ecbee4f3ad
James Davis <james.jpdavis@gmail.com>
2016-08-11 22:42:28 -0400

    worked on movie func

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '24Ha1v3nNXfbBg5d8RMWjv5ui',
+  consumer_secret: 'OXqteUvdW2nHEYpmjGPJNUBIVTxvWYqQ1SBhA865Z1Xc9nlMQ5',
+  access_token_key: '4858020490-a0gugJCpYJQMQJXQA2Yy0UhCrGGb1YM2yt89G3A',
+  access_token_secret: 'kVJlCONHWwnPJhBFL2J7NkTACMs0adRG2odsLRZDkWPOK',
+};
